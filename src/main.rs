929e71d7b26601f2764f8fc63e51982635648be7
vimwitch <jchancehud@gmail.com>
2024-08-30 17:24:56 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ extern crate merlin;
 use curve25519_dalek::scalar::Scalar;
 use libspartan::{InputsAssignment, Instance, SNARKGens, VarsAssignment, SNARK};
 use merlin::Transcript;
-use rand::rngs::OsRng;
 
 const PROGRAM: &'static str = "
 let x = 4
@@ -102,10 +101,12 @@ fn produce_tiny_r1cs(
         panic!("error building witness: {:?}", e);
     }
     let mut witness = witness.unwrap();
+    // put the one variable at the end of the witness vector
     let l = witness.len();
     witness[0] = witness[l - 1];
     witness[l - 1] = Curve25519FieldElement::from(1);
     let constraints;
+    // filter out the symbolic constraints
     {
         let r1cs_parser: R1csParser<Curve25519FieldElement> = R1csParser::new(r1cs);
         constraints = r1cs_parser
@@ -115,9 +116,13 @@ fn produce_tiny_r1cs(
             .collect::<Vec<_>>();
     }
 
+    // number of constraints
     let num_cons = constraints.len();
+    // number of variables
     let num_vars = witness.len() - 1;
     let num_inputs = 0;
+    let num_non_zero_entries = witness.len() - 1;
+
     // in each constraint remap the one variable to the end of the
     // var vector
     let remapped_constraints = constraints
@@ -164,7 +169,6 @@ fn produce_tiny_r1cs(
             }
         })
         .collect::<Vec<_>>();
-    let num_non_zero_entries = witness.len() - 1;
 
     // create a VarsAssignment
     let mut vars = vec![Scalar::ZERO.to_bytes(); num_vars];
@@ -186,43 +190,6 @@ fn produce_tiny_r1cs(
             C.push((i, *col_i, to_32(v.to_bytes_le())));
         }
     }
-    // We will use the following example, but one could construct any R1CS instance.
-    // Our R1CS instance is three constraints over five variables and two public inputs
-    // (Z0 + Z1) * I0 - Z2 = 0
-    // (Z0 + I1) * Z2 - Z3 = 0
-    // Z4 * 1 - 0 = 0
-
-    // The constraint system is defined over a finite field, which in our case is
-    // the scalar field of ristreeto255/curve25519 i.e., p =  2^{252}+27742317777372353535851937790883648493
-    // To construct these matrices, we will use `curve25519-dalek` but one can use any other method.
-
-    // a variable that holds a byte representation of 1
-    // let one = Scalar::ONE.to_bytes();
-
-    // R1CS is a set of three sparse matrices A B C, where is a row for every
-    // constraint and a column for every entry in z = (vars, 1, inputs)
-    // An R1CS instance is satisfiable iff:
-    // Az \circ Bz = Cz, where z = (vars, 1, inputs)
-
-    // constraint 0 entries in (A,B,C)
-    // constraint 0 is (Z0 + Z1) * I0 - Z2 = 0.
-    // We set 1 in matrix A for columns that correspond to Z0 and Z1
-    // We set 1 in matrix B for column that corresponds to I0
-    // We set 1 in matrix C for column that corresponds to Z2
-    // A.push((0, 0, one));
-    // A.push((0, 1, one));
-    // B.push((0, num_vars + 1, one));
-    // C.push((0, 2, one));
-
-    // constraint 1 entries in (A,B,C)
-    // A.push((1, 0, one));
-    // A.push((1, num_vars + 2, one));
-    // B.push((1, 2, one));
-    // C.push((1, 3, one));
-
-    // constraint 3 entries in (A,B,C)
-    // A.push((2, 4, one));
-    // B.push((2, num_vars, one));
 
     let inst = Instance::new(num_cons, num_vars, num_inputs, &A, &B, &C);
     if let Err(e) = inst {
@@ -230,27 +197,10 @@ fn produce_tiny_r1cs(
     }
     let inst = inst.unwrap();
 
-    // compute a satisfying assignment
-    // let mut csprng: OsRng = OsRng;
-    // let i0 = Scalar::random(&mut csprng);
-    // let i1 = Scalar::random(&mut csprng);
-    // let z0 = Scalar::random(&mut csprng);
-    // let z1 = Scalar::random(&mut csprng);
-    // let z2 = (z0 + z1) * i0; // constraint 0
-    // let z3 = (z0 + i1) * z2; // constraint 1
-    // let z4 = Scalar::ZERO; //constraint 2
-
-    // vars[0] = z0.to_bytes();
-    // vars[1] = z1.to_bytes();
-    // vars[2] = z2.to_bytes();
-    // vars[3] = z3.to_bytes();
-    // vars[4] = z4.to_bytes();
     let assignment_vars = VarsAssignment::new(&vars).unwrap();
 
     // create an InputsAssignment
     let inputs = vec![Scalar::ZERO.to_bytes(); num_inputs];
-    // inputs[0] = i0.to_bytes();
-    // inputs[1] = i1.to_bytes();
     let assignment_inputs = InputsAssignment::new(&inputs).unwrap();
 
     // check if the instance we created is satisfiable
